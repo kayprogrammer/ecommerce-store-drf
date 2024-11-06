9a742f4dfdea9e3339c931d10ed8107774dc80b3
kene <kenechiifeanyi@gmail.com>
2024-11-06 14:39:43 +0100

    tests: profile retrieval

--- apps/profiles/tests.py
+++ apps/profiles/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from apps.accounts.test_utils import TestAccountUtil
+
+
+class TestProfiles(APITestCase):
+    base_url = "/api/v1/profiles"
+
+    maxDiff = None
+
+    def setUp(self):
+        self.user = TestAccountUtil.new_user()
+        auth_token = TestAccountUtil.auth_token(self.user)
+        self.bearer = {"HTTP_AUTHORIZATION": f"Bearer {auth_token}"}
+
+    def test_retrieve_profile(self):
+        user = self.user
+        response = self.client.get(f"{self.base_url}/", **self.bearer)
+        self.assertEqual(response.status_code, 200)
+        result = response.json()
+        self.assertEqual(result["status"], "success")
+        self.assertEqual(result["message"], "User Profile Fetched")
+        self.assertEqual(
+            result["data"],
+            {
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "email": user.email,
+                "avatar_url": user.avatar_url,
+                "account_type": user.account_type,
+            },
+        )
